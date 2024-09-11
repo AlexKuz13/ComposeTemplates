972e5df1ffcfc08365d821eca6601173e7072567
AlexKuz13 <kuznetsov356@gmail.com>
2024-09-11 19:19:53 +0300

    add demo action

--- src/main/kotlin/com/alexkuz/composeteamplates/DemoAction.kt
+++ src/main/kotlin/com/alexkuz/composeteamplates/DemoAction.kt
@@ -1,4 +1,16 @@
 package com.alexkuz.composeteamplates
 
-class DemoAction {
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.ui.Messages
+
+class DemoAction(): AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+        Messages.showMessageDialog(
+            e.project,
+            "Great! You just created your first action!",
+            "My First Action",
+            Messages.getInformationIcon())
+    }
 }
