3cc5cf18a30a6435f37b8d242cbc1ad2511df5da
AlexKuz13 <kuznetsov356@gmail.com>
2024-09-15 22:25:48 +0300

    change idea to android studio

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,10 +14,8 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2023.2.6")
-    type.set("IC") // Target IDE Platform
-
-    plugins.set(listOf(/* Plugin Dependencies */))
+    localPath = "/Applications/Android Studio.app/Contents"
+   // plugins.set(listOf("Kotlin", "Android"))
 }
 
 tasks {
