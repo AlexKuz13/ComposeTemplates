9d5187da68c6cd0c28242c974f1aa22cd242d03d
Александр Кузнецов <kuznetsov356@gmail.com>
2025-01-18 22:00:19 +0300

    add changeNote

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.alexkuz"
-version = "1.0-SNAPSHOT"
+version = "1.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -31,6 +31,7 @@ tasks {
     patchPluginXml {
         sinceBuild.set("232")
         untilBuild.set("242.*")
+        changeNotes = "This is the first version of the plugin."
     }
 
     signPlugin {
